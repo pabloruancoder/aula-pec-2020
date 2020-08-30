fe7ecf6d41a98ccd0f013aa727dbaa87785c28d5
Pablo Ruan <colopip808@gmail.com>
2020-08-30 16:05:42 -0300

    atividade t1

---
+++ 003atividade01.py
@@ -0,0 +1,6 @@
+nome = input()
+sexo = int(input())
+if sexo == 1:
+    print(f'Ilmo Sr. {nome}')
+if sexo == 2:
+    print(f'Ilma Sra. {nome}')
---
+++ 003atividade02.py
@@ -0,0 +1,7 @@
+numero = int(input())
+resultado = numero % 2
+if resultado != 0:
+    print(bool(resultado))
+else:
+    print(bool(resultado))
+        
