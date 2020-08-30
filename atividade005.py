e66a6b9c952154fff4c3f3c9f066f9e1d084a2f2
Pablo Ruan <colopip808@gmail.com>
2020-08-30 15:50:48 -0300

    modifiquei

--- atividade005.py
+++ atividade005.py
@@ -1,7 +1,10 @@
-valor = float(input())
-desconto = (valor * 9)/100
-print(f'{desconto}')
-valor01 = valor * 5
-print(f'{valor01}')
-valor02 = ((valor * 10)+(valor * 17)/100)
-print(f'{valor2}')
+def numero1(desconto, prestimo1, prestimo2):
+    def main():
+            numero1 = float(input())
+            desconto = (numero1 * 0.9)
+            prestimo1 = (numero1 * 5)
+            prestimo2 = (numero1 * 10 * 0.17)
+
+print("{},{},{}".format(desconto,prestimo1,prestimo2))
+if __name__=="__main__":
+        main()
